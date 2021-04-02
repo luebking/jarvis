bae465e2a8a1c04c7e713239811186178f7cf49b
Thomas Lübking <thomas.luebking@gmail.com>
2021-04-02 13:38:57 +0200

    create paths

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ chmod 777 ${DESTDIR}/usr/share/jarvis/.cache
 touch ${DESTDIR}/usr/share/jarvis/.zdirs
 chmod 666 ${DESTDIR}/usr/share/jarvis/.zdirs
 install -v -m 444 zshrc ${DESTDIR}/usr/share/jarvis/.zshrc
+install -d -v ${DESTDIR}/usr/share/jarvis/funcs.d
 install -v -m 444 funcs.d/* ${DESTDIR}/usr/share/jarvis/funcs.d
 install -v -m 444 README.md img/* ${DESTDIR}/usr/share/jarvis/
-install -v jarvis ${DESTDIR}/usr/bin/jarvis
+install -D -v jarvis ${DESTDIR}/usr/bin/jarvis
